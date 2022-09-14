754ab554c3c3a3c1a0e4fa38635af6f49e2db887
Matthias Tylkowski <tykomat@b-tu.de>
2022-09-14 11:27:21 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { ActionData } from './$types';
+
+  export let form: ActionData;
+</script>
+
+<h1>Click inside the input element and hit enter</h1>
+<form method="post" action="/">
+  <input type="text" name="test">
+</form>
